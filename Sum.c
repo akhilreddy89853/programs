7757e03e6fe138722b567f44c6b0867f2ceb1a5d
Akhil Reddy <some1234@gmail.com>
2019-01-31 12:20:28 +0530

    New file added

---
+++ Sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int Number1 = 0, Number2 = 0;
+    int Sum = 0; 
+    printf("To add two numbers, enter the firt number: ");
+    scanf("%d",  &Number1);
+    printf("Enter the second number: ");
+    scanf("%d",  &Number2);
+    sum = Number1 + Number2;
+    Printf("The sum of  %d and %d is %d.", Number1, Number2, sum);
+    return 0;
+}
